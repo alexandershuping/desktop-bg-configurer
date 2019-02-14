e34398638b07cd7ec9a350c1c15dc7cfeb3ddcb5
Alexander Shuping <alexandershuping@gmail.com>
2019-02-14 13:55:47 -0500

    Improvements to directory detection

--- import.sh
+++ import.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-DN=$(dirname "$0")
-BN=$(basename "$0")
+FILES_DIRECTORY=$( cd "$(dirname "$0")" ; pwd -P )
 
-SYM_DIRECTORY=$DN
-FILES_DIRECTORY=$(readlink $SYM_DIRECTORY/$BN)
-
-if [ -n "$FILES_DIRECTORY" ];then
-	echo "File is symlinked. Finding base path..."
-	FILES_DIRECTORY=$(echo "$FILES_DIRECTORY" | sed 's/\/[^\/]*$//')
-else
-	FILES_DIRECTORY=$SYM_DIRECTORY
-fi
+echo "base: $FILES_DIRECTORY"
 
 for image in $(ls $FILES_DIRECTORY/to_add | sed -n '/README/!p');do
 	mv $FILES_DIRECTORY/to_add/$image $FILES_DIRECTORY/all/$image
-	ln -s ../all/$image ./active/$image
+	ln -s $FILES_DIRECTORY/all/$image $FILES_DIRECTORY/active/$image
 done
--- select_background.sh
+++ select_background.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-DN=$(dirname "$0")
-BN=$(basename "$0")
-
-SYM_DIRECTORY=$DN
-FILES_DIRECTORY=$(readlink $SYM_DIRECTORY/$BN)
-
-if [ -n "$FILES_DIRECTORY" ];then
-	echo "File is symlinked. Finding base path..."
-	FILES_DIRECTORY=$(echo "$FILES_DIRECTORY" | sed 's/\/[^\/]*$//')
-else
-	FILES_DIRECTORY=$SYM_DIRECTORY
-fi
+FILES_DIRECTORY=$( cd "$(dirname "$0")" ; pwd -P )
 
 CHOSEN_IMAGE=$(ls $FILES_DIRECTORY/active | sed -n '/README/!p' | shuf -n 1)
 echo "chose $FILES_DIRECTORY/active/$CHOSEN_IMAGE."
