03dd2d2a4853bb6a9f6c78572942db1aaa854565
Alexander Shuping <alexandershuping@gmail.com>
2019-02-14 14:02:04 -0500

    Final fix to symlink detection

--- import.sh
+++ import.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-FILES_DIRECTORY=$( cd "$(dirname "$0")" ; pwd -P )
+DN=$(dirname "$0")
+BN=$(basename "$0")
 
-echo "base: $FILES_DIRECTORY"
+SYM_DIRECTORY=$DN
+FILES_DIRECTORY=$(readlink $SYM_DIRECTORY/$BN)
+
+if [ -n "$FILES_DIRECTORY" ];then
+	echo "File is symlinked. Finding base path..."
+	FILES_DIRECTORY=$(echo "$FILES_DIRECTORY" | sed 's/\/[^\/]*$//')
+else
+	FILES_DIRECTORY=$SYM_DIRECTORY
+fi
+
+FILES_DIRECTORY=$(cd $FILES_DIRECTORY; pwd -P)
+echo "Base directory: $FILES_DIRECTORY"
 
 for image in $(ls $FILES_DIRECTORY/to_add | sed -n '/README/!p');do
 	mv $FILES_DIRECTORY/to_add/$image $FILES_DIRECTORY/all/$image
